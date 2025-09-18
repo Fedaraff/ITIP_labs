a9cbba82d278292bdec1ab5cdb5efa6a3da596c9
m1nesty <fnp06@mail.ru>
2025-09-18 21:34:10 +0300

    lab_1

---
+++ src/Primes.java
@@ -0,0 +1,19 @@
+public class Primes {
+    public static void main(String[] args) {
+        System.out.println("Простые числа меньше 100:");
+
+        for (int num = 2; num < 100; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
